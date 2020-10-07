7f228859149e48ce78f0762b822b1e9cf5bfb3b1
kyeswanth <yeswanthk@biarca.com>
2020-10-07 19:18:12 +0530

    Display vulnerabilities count

--- trivy/trivy-script.sh
+++ trivy/trivy-script.sh
@@ -13,17 +13,21 @@ function trivy_scan(){
     # Get image name from SHA ID
     image_name=$(docker inspect "${image_sha_id}" | jq -r '.[0].RepoTags | first')
     # Run trivy to fetch high,critical vulnerability details for each image
-    trivy --exit-code 1 --severity HIGH,CRITICAL "${image_name}"
+    trivy --exit-code 1 --quiet --severity HIGH,CRITICAL "${image_name}"
     EXITCODE=$?
     if [ $EXITCODE -eq 1 ]; then
       # Write vulnerability details to log file
-      trivy "${image_name}" >> trivy_report.log
+      trivy --quiet --severity HIGH,CRITICAL "${image_name}" >> trivy_report.log
+      trivy --quiet --severity HIGH,CRITICAL -f json "${image_name}" >> trivy_report.json
     fi
   done
 
+  high_count=$(cat trivy_report.json | jq '.[]' | jq '.Vulnerabilities[] | select(.Severity == "HIGH") | length' | wc -l)
+  critical_count=$(cat trivy_report.json | jq '.[]' | jq '.Vulnerabilities[] | select(.Severity == "CRITICAL") | length' | wc -l)
+
   if [ -s /trivy_report.log ]; then
     # Trigger slack alert if vulnerabilities found
-    curl -F file=@trivy_report.log -F "initial_comment=Trivy report for $KUBE_CONTEXT on `date`" -F channels=$TRIVY_SLACK_CHANNEL_ID -H "Authorization: Bearer `$TRIVY_SLACK_TOKEN`" https://slack.com/api/files.upload
+    curl -F file=@trivy_report.log -F "title=Trivy detected $high_count HIGH and $critical_count CRITICAL vulnerabilities in $KUBE_CONTEXT on `date`" -F channels=$TRIVY_SLACK_CHANNEL_ID -H "Authorization: Bearer `$TRIVY_SLACK_TOKEN`" https://slack.com/api/files.upload
   fi
 }
 
